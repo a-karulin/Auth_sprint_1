8d4d6a8ed41ead98caa1dc93350690779086b4b6
dmitriyfedotov <fedotovdmitriy14@gmail.com>
2023-04-16 01:19:59 +0300

    added docker-compose

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,16 +1,27 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flasgger import Swagger
-from flask_app.api.v1.auth import auth
-from flask_app.api.v1.roles import roles
-from flask_app.api.v1.users import users
 
+from api.v1.auth import auth
+from api.v1.roles import roles
+from api.v1.users import users
+from config import POSTGRES_CONN_STR
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = POSTGRES_CONN_STR
 
-Swagger(app, template_file="project-description/openapi.yaml")
+Swagger(app, template_file="swagger/openapi.yaml")
 SQLAlchemy(app)
 
 app.register_blueprint(auth, url_prefix="/api/v1/auth")
 app.register_blueprint(roles, url_prefix="/api/v1/roles")
 app.register_blueprint(users, url_prefix="/api/v1/users")
+
+
+@app.route('/')
+def get_status():
+    return {'status': 'ok'}
+
+
+if __name__ == '__main__':
+    app.run()
