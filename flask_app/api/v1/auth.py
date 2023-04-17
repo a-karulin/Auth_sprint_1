ed6497491cec3dfeaf803711689b8867e058a336
Александр Карулин <av.karulin@tensor.ru>
2023-04-17 22:49:42 +0300

    logout

--- flask_app/api/v1/auth.py
+++ flask_app/api/v1/auth.py
@@ -1,6 +1,19 @@
-from flask import Blueprint
+from datetime import timedelta
+
+from flask import Blueprint, jsonify
+from flask_jwt_extended import get_jwt, jwt_required
+import redis
 
 auth = Blueprint("auth", __name__)
+
+# Setup our redis connection for storing the blocklisted tokens. You will probably
+# want your redis instance configured to persist data to disk, so that a restart
+# does not cause your application to forget that a JWT was revoked.
+jwt_redis_blocklist = redis.StrictRedis(
+    host="localhost", port=6379, db=0, decode_responses=True
+)
+
+ACCESS_EXPIRES = timedelta(hours=1)
 
 
 @auth.route("/signup", methods=["POST"])
@@ -13,9 +26,17 @@ def login_user():
     pass
 
 
-@auth.route("/logout")
+@auth.route("/logout", methods=["DELETE"])
+@jwt_required()
 def logout():
-    pass
+    token = get_jwt()
+    jti = token["jti"]
+
+    jwt_redis_blocklist.set(jti, "", ex=ACCESS_EXPIRES)
+
+    # Returns "Access token revoked" or "Refresh token revoked"
+
+    return jsonify(msg=f"Access token revoked")
 
 
 @auth.route("/refresh", methods=["POST"])
