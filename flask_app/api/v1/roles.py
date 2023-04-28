be0ae396fe19cfcc39f8287ed6c4f178204cd83f
Александр Карулин <av.karulin@tensor.ru>
2023-04-28 18:25:37 +0300

    roles api in swagger

--- flask_app/api/v1/roles.py
+++ flask_app/api/v1/roles.py
@@ -3,6 +3,7 @@ from flask_jwt_extended import jwt_required
 
 from database.db_models import Roles
 from services.role import RoleService
+from services.tokens import admin_access
 
 roles = Blueprint("roles", __name__)
 
