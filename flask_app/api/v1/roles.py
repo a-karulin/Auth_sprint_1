de754b543c67bebec19c104fd690386f5ab55867
Александр Карулин <av.karulin@tensor.ru>
2023-04-28 18:03:44 +0300

    admin access for role api

--- flask_app/api/v1/roles.py
+++ flask_app/api/v1/roles.py
@@ -9,12 +9,14 @@ roles = Blueprint("roles", __name__)
 
 @roles.route("/", methods=["GET"])
 @jwt_required()
+@admin_access()
 def roles_list():
     return [Roles(id=role_item.id, role=role_item.role) for role_item in Roles.query.all()]
 
 
 @roles.route("/create", methods=["POST"])
 @jwt_required()
+@admin_access()
 def create_role():
     role_service = RoleService()
     response, http_status = role_service.create_role(request.json.get('role'))
@@ -22,6 +24,7 @@ def create_role():
 
 
 @roles.route("/<role_id>", methods=["PATCH"])
+@admin_access()
 def update_role():
     role_id = request.args.get('user_id')
     new_role_name = request.json.get('role')
@@ -31,6 +34,7 @@ def update_role():
 
 
 @roles.route("/<role_id>", methods=["DELETE"])
+@admin_access()
 def delete_role():
     role_id = request.args.get('user_id')
     role_service = RoleService()
