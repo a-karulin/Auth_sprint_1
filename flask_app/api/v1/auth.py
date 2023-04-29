37ae4dd1e23f2e3ead09ffd8b747c58dc81b0355
dmitriyfedotov <fedotovdmitriy14@gmail.com>
2023-04-30 00:07:10 +0300

    added roles to token payload

--- flask_app/api/v1/auth.py
+++ flask_app/api/v1/auth.py
@@ -26,13 +26,14 @@ def handle_invalid_creds_request(error):
 
 @auth.route("/signup", methods=["POST"])
 def create_user():
-    db = UserService()
-    new_user = db.register_user(
+    user_service = UserService()
+    new_user = user_service.register_user(
         login=request.json.get('login'),
         password=request.json.get('password'),
         last_name=request.json.get('last_name'),
         first_name=request.json.get('first_name'),
     )
+    new_user['roles'] = user_service.get_roles_names_for_user(new_user['id'])
     access_token, refresh_token = create_access_and_refresh_tokens(
         identity=new_user['id'],
         payload=new_user,
@@ -53,6 +54,7 @@ def login_user():
         password=request.json.get('password', None),
         user_agent=request.headers.get("user-agent", ""),
     )
+    user['roles'] = user_service.get_roles_names_for_user(user['id'])
     access_token, refresh_token = create_access_and_refresh_tokens(
         identity=user['id'],
         payload=user,
@@ -84,8 +86,12 @@ def refresh_tokens():
     redis_storage = RedisTokenStorage()
     if redis_storage.check_token_in_blacklist(token) is None:
         user = user_service.get_user_by_id(user_id)
+        user['roles'] = user_service.get_roles_names_for_user(user['id'])
         redis_storage.add_token_to_blacklist(token)
-        access_token, refresh_token = create_access_and_refresh_tokens(user_id, user)
+        access_token, refresh_token = create_access_and_refresh_tokens(
+            identity=user_id,
+            payload=user,
+        )
         response = {
             'access_token': access_token,
             'refresh_token': refresh_token,
