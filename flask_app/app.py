01cd70018dfbfc48022b7956da534e0e62b5f6f8
Александр Карулин <av.karulin@tensor.ru>
2023-05-02 23:03:41 +0300

    jaeger tracer

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,5 +1,5 @@
 import click
-from flask import Flask
+from flask import Flask, request
 from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
 from flasgger import Swagger
@@ -11,6 +11,12 @@ from config import POSTGRES_CONN_STR, JWT_SECRET_KEY, JWT_ALGORITHM
 from flask_swagger_ui import get_swaggerui_blueprint
 
 from services.user import UserService
+
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = POSTGRES_CONN_STR
@@ -48,10 +54,33 @@ app.register_blueprint(swagger_blueprint)
 app.cli.add_command(create_superuser)
 
 
+@app.before_request
+def before_request():
+    request_id = request.headers.get('X-Request-Id')
+    if not request_id:
+        raise RuntimeError('request id is required')
+
+
+def configure_tracer() -> None:
+    trace.set_tracer_provider(TracerProvider())
+    trace.get_tracer_provider().add_span_processor(
+        BatchSpanProcessor(
+            JaegerExporter(
+                agent_host_name='localhost',
+                agent_port=6831,
+            )
+        )
+    )
+    # Чтобы видеть трейсы в консоли
+    trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(ConsoleSpanExporter()))
+
+
 @app.route('/')
 def get_status():
     return {'status': 'ok'}
 
 
 if __name__ == '__main__':
+    configure_tracer()
     app.run()
+    FlaskInstrumentor().instrument_app(app)
